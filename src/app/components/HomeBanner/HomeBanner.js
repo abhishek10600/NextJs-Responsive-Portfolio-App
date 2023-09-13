edfb89be5835480e7eb5998432ec8697abd6c8c5
abhishek10600 <abhi10sheksharma@gmail.com>
2023-09-13 20:53:08 +0530

    Third Commit.

--- src/app/components/HomeBanner/HomeBanner.js
+++ src/app/components/HomeBanner/HomeBanner.js
@@ -7,7 +7,7 @@ const HomeBanner = () => {
     return (
         <div className="flex items-center justify-center h-[250px] md:h-[900px] gap-3 md:gap-[100px] bg-[#282828] text-white px-6 py-8" id={"Home"}>
             <div className="left flex flex-col gap-3 md:gap-8 justify-center items-center md:ml-32">
-                <h1 className="md:text-7xl text-base font-bold">Full Stack Developer</h1>
+                <h1 className="md:text-7xl text-sm font-bold">Full Stack Developer</h1>
                 <h2 className="md:text-2xl text-xs text-center">Building Websites and Web Applications for you!</h2>
                 <Link href={"https://github.com/abhishek10600"} className={"px-1 py-1 md:px-4 md:py-2  border text-xs md:border-2 md:text-xl hover:bg-white hover:text-black transition duration-180 ease-out hover:ease-in"} target={"_blank"}>Visit Github Profile</Link>
             </div>
